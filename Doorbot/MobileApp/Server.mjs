a0f4f2e08e010a52eb07e3a5d6ecc374c14fcb79
AlexTom <alexanderjtom06@gmail.com>
2026-06-12 01:45:48 -0400

    Added token and websocket features. Websocket now is used after login to send and recieve commands. Added a hardware role to allow ESP32 an untimed token

--- Doorbot/MobileApp/Server.mjs
+++ Doorbot/MobileApp/Server.mjs
@@ -1,7 +1,9 @@
 import express from 'express';
 import fs from 'node:fs/promises';
+import url from 'node:url';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const app = express();
 const port = Secrets.env.PORT;
@@ -9,13 +11,8 @@ const secretKey = Secrets.env.JWT_SECRET;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log(`Connection received at: ${req.url}`);
-    res.json({ message: "Hello from your new Express server!" });
-});
-
 app.post('/login', async (req,res) => {
-    const{username, password} = req.body;
+    const{username, password, role} = req.body;
     const rawData = await fs.readFile('Users.json', 'utf8');
     const dataArray = JSON.parse(rawData);
 
@@ -43,18 +40,76 @@ app.post('/login', async (req,res) => {
     }
     console.log(`Login successful for "${acceptedUser.username}"`);
 
-    const token = jwt.sign(
-        {userId: acceptedUser.username}, 
-        process.env.JWT_SECRET, 
-        {expiresIn: '10m'}
-    );
+
+    let token;
+    if(acceptedUser.role === 'hardware'){
+        token = jwt.sign(
+            {userId: acceptedUser.username, role: acceptedUser.role}, 
+            process.env.JWT_SECRET
+        );
+    } else{
+        token = jwt.sign(
+            {userId: acceptedUser.username, role: acceptedUser.role}, 
+            process.env.JWT_SECRET, 
+            {expiresIn: '10m'}
+        );
+    }
+
     return res.status(200).json({
         message: "Access granted!",
         token: token
     });
 })
 
+const wss = new WebSocketServer({noServer: true});
+const activeConnections = new Map();
+
+wss.on('connection', (ws, req) => {
+    try{
+        const parsedUrl = url.parse(req.url, true);
+        const token = parsedUrl.query.token;
+
+        if(!token){
+            console.log("Missing token");
+            ws.close(4001, "Authentication token missing");
+            return;
+        }
+
+        const decodedPayload = jwt.verify(token, process.env.JWT_SECRET);
+        const userRole = decodedPayload.role; 
+        const currentUserId = decodedPayload.userId;
+        activeConnections.set(userRole, ws);
+
+
+        ws.on('message',(rawMessage) =>{
+            const cleanMessage = JSON.parse(rawMessage);
+            console.log(`Client recieved message ${cleanMessage}`);
+
+            const ESP32 = activeConnections.get('hardware');
+
+            if(cleanMessage.input === "UNLOCK"){
+                const payload = {command: "UNLOCK" };
+                ws.send(JSON.stringify(payload));
+                if(ESP32){
+                    ESP32.send(JSON.stringify(payload))
+                }
+            }
+        });
+
+    }
+    catch{
+        console.error("Invalid or expired token");
+        ws.close(4002, "Authentication failed");
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
+});
+
+server.on('upgrade', (request, socket, head) =>{
+    wss.handleUpgrade(request, socket, head, (wsSocket) =>{
+        wss.emit('connection', wsSocket, request);
+    });
 });
