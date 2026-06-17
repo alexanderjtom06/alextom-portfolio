ba51bdf0def81bd912489c79806313ab6eb063d8
AlexTom <alexanderjtom06@gmail.com>
2026-06-16 23:49:02 -0400

    Removed swift development and started basic web page for phone. HTML only. Made rough draft of a login page.

---
+++ Doorbot/MobileApp/Doorbot/Doorbot/DoorbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorbotApp.swift
+//  Doorbot
+//
+//  Created by Alex Tom on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct DoorbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
